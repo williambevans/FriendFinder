ebb16ca10a2a7c1e15302080e38039d2c64d9890
WHBJ <bevansrealtors@gmail.com>
2019-03-27 17:46:48 -0500

    unfinished application

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,24 @@
+var peopleData = require("../data/friends");
+
+
+module.exports = function (app) {
+
+
+    app.get("/api/friends", function (req, res) {
+        res.json(peopleData);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        // req.body is available since we're using the body parsing middleware
+        if (peopleData.length < 5) {
+            peopleData.push(req.body);
+            res.json(true);
+        } else {
+            alert('something went wrong, please return to home page.')
+        }
+    });
+};
+
+console.log(peopleData);
